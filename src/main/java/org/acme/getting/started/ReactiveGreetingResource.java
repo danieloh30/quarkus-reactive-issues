7ce6b0f99481168c52f684f34481ed66c89582ad
danieloh30 <doh@redhat.com>
2021-10-21 14:22:02 -0400

    Add code

---
+++ src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
@@ -0,0 +1,50 @@
+package org.acme.getting.started;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+@Path("/hello")
+public class ReactiveGreetingResource {
+
+    @Inject
+    ReactiveGreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Object hello() {
+        return "hello";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public Uni<String> greeting(String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{count}/{name}")
+    public Multi<String> greetings(int count, String name) {
+        return service.greetings(count, name);
+    }
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType(MediaType.TEXT_PLAIN)
+    @Path("/stream/{count}/{name}")
+    @Blocking
+    public Multi<String> greetingsAsStream(int count, String name) {
+        return service.greetings(count, name);
+    }
+
+}
---
+++ src/main/java/org/acme/getting/started/graphql/LightSaber.java
@@ -0,0 +1,5 @@
+package org.acme.getting.started.graphql;
+
+enum LightSaber {
+    RED, BLUE, GREEN, PURPLE
+}
